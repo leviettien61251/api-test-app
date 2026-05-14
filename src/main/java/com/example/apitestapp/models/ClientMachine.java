d707b337e004fa74d8ac7bdd3f07f17536a9add9
Lê Việt Tiến <levieitien5744@gmail.com>
2026-05-14 18:53:40 +0700

    save client machine info when login

--- src/main/java/com/example/apitestapp/models/ClientMachine.java
+++ src/main/java/com/example/apitestapp/models/ClientMachine.java
@@ -16,11 +16,8 @@ public class ClientMachine {
     private String id;
     private String userId;
     private String machineName;
-    private String osName;         // Windows, macOS, Linux
-    private String osVersion;      // 10 Pro 22H2, 14.3 Sonoma
-    private String ipAddress;      // IPv4 hoặc IPv6
-    private String cpuInfo;        // Intel Core i7-13700K
-    private String ramInfo;        // 32GB DDR5
+    private String os;
+    private String ipAddress;
     private String hostname;
 
     @Builder.Default
